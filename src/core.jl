02063190a9f5565dfbd3fb4ceb0bd7d617b95a13
sstroemer <stefan.stroemer@gmail.com>
2024-10-11 16:51:59 +0200

    fix: remove explicit name from Base.show

--- src/core.jl
+++ src/core.jl
@@ -110,7 +110,7 @@ _finalize_docstring(Profile)
 _finalize_docstring(Unit)
 
 function Base.show(io::IO, cc::_CoreComponent)
-    str_show = """An $(typeof(cc)): '$(cc.name)'"""
+    str_show = """An $(typeof(cc)):"""
     
     fields = _result_fields(cc)
     for field in fields[1:(end-1)]
