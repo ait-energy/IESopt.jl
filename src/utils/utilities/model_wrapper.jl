eefe0977f9ec313803e9223093e42d29702970d1
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-10-16 13:38:17 +0200

    fix: make iesopt_model field in ModelWrapper "private"

--- src/utils/utilities/model_wrapper.jl
+++ src/utils/utilities/model_wrapper.jl
@@ -9,14 +9,16 @@ Wraps an IESopt.jl model to expose various easy access functions in `IESopt.Util
 - `yearspan::Float64`: The total timespan of the model, in years, based on `1 year = 8760 hours`.
 """
 struct ModelWrapper
-    iesopt_model::JuMP.Model
+    _iesopt_model::JuMP.Model
 end
 
 function Base.getproperty(mw::ModelWrapper, property::Symbol)
+    _iesopt_model = getfield(mw, :_iesopt_model)
+
     if property == :model
-        return getfield(mw, :iesopt_model)
+        return _iesopt_model
     elseif property == :timespan
-        return sum(s.weight for s in values(IESopt._iesopt_model(mw.model).snapshots))
+        return sum(s.weight for s in values(IESopt._iesopt_model(_iesopt_model).snapshots))
     elseif property == :yearspan
         return mw.timespan / 8760.0
     end
