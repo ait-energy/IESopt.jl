32018a65d3ccf2841206339bc082ec130e246e6b
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-11-04 13:01:05 +0100

    chore: minor update to precompile workload

--- src/precompile/precompile_tools.jl
+++ src/precompile/precompile_tools.jl
@@ -4,12 +4,17 @@
 
     @compile_workload begin
         if !isnothing(Library)
-            model = IESopt.generate!(normpath(dir, "01_basic_single_node.iesopt.yaml"); verbosity=false)
-            IESopt.optimize!(model)
+            model = generate!(normpath(dir, "01_basic_single_node.iesopt.yaml"); verbosity=false)
+            optimize!(model)
 
-            IESopt.generate!(normpath(dir, "08_basic_investment.iesopt.yaml"); verbosity=false)
-            IESopt.generate!(normpath(dir, "09_csv_only.iesopt.yaml"); verbosity=false)
-            IESopt.generate!(normpath(dir, "46_constants_in_objective.iesopt.yaml"); verbosity=false)
+            generate!(normpath(dir, "08_basic_investment.iesopt.yaml"); verbosity=true)
+            
+            model = generate!(normpath(dir, "09_csv_only.iesopt.yaml"); verbosity=false)
+            optimize!(model)
+
+            generate!(normpath(dir, "46_constants_in_objective.iesopt.yaml"); verbosity=false)
         end
     end
 end
+
+precompile(_attach_optimizer, (JuMP.Model,))
