3535233a14da72e84e72282d73fe0de3b74665eb
sstroemer <stefan.stroemer@gmail.com>
2024-11-05 20:30:22 +0100

    test: fix imports

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,15 @@
 using Test, TestItemRunner
-import Aqua, JET
+import Aqua, JET, Suppressor
+import JuMP, HiGHS
 using IESopt
-import JuMP, HiGHS, Suppressor
 
 const PATH_EXAMPLES = IESopt.Assets.get_path("examples")
 const PATH_TESTFILES = normpath(@__DIR__, "test_files")
 const PATH_CURRENT = pwd()
 
 @testset "IESopt.jl" verbose = true begin
-    @testset "Code quality" begin
-        @testset "Aqua.jl" begin
+    @testset "Code quality" verbose = true begin
+        @testset "Aqua.jl" verbose = true begin
             include("src/aqua.jl")
         end
 
@@ -18,15 +18,15 @@ const PATH_CURRENT = pwd()
         end
     end
 
-    @testset "Unit tests (IESopt.jl)" begin
+    @testset "Unit tests (IESopt.jl)" verbose = true begin
         @run_package_tests filter = ti -> (:unittest in ti.tags)
     end
 
-    @testset "Basic (IESopt.jl)" begin
+    @testset "Basic (IESopt.jl)" verbose = true begin
         include("src/basic.jl")
     end
 
-    @testset "Examples (IESopt.jl)" begin
+    @testset "Examples (IESopt.jl)" verbose = true begin
         # include("src/examples.jl")
     end
 end
