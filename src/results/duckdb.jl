21b9fc81e77a7a3a95e23b3217eaa68dd2248ee2
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-11-13 02:20:39 +0100

    fix: slight modification in typing of constraint refs

--- src/results/duckdb.jl
+++ src/results/duckdb.jl
@@ -62,10 +62,10 @@ function append_component_result!(
     cid::Int64,
     rtid::Int64,
     rid::Int64,
-    entry::JuMP.ConstraintRef;
+    entry::JuMP.ConstraintRef{M, C, S};
     has_duals::Bool,
     sid::Int64=-1,
-)
+) where {M, C, S}
     has_duals || return nothing
 
     append_component_info!(appender, cid, rtid, rid, sid, 4)
@@ -82,9 +82,9 @@ function append_component_result!(
     rid::Int64,
     entry::Vector{T};
     has_duals::Bool,
-) where {T <: Union{JuMP.AffExpr, JuMP.VariableRef, JuMP.ConstraintRef}}
+) where {T}
     for (t, item) in enumerate(entry)
-        append_component_result!(appender, cid, rtid, rid, item; has_duals, sid=t)
+        append_component_result!(appender, cid, rtid, rid, item::T; has_duals, sid=t::Int64)
     end
 end
 
@@ -94,7 +94,7 @@ function db_create_table(db::DuckDB.DB, model::JuMP.Model, ::AttrExtractObjectiv
     appender = DuckDB.Appender(db, "objectives")
     for (name, objective) in IESopt._iesopt_model(model).objectives
         DuckDB.append(appender, name)
-        DuckDB.append(appender, JuMP.value(objective.expr))
+        DuckDB.append(appender, JuMP.value(objective.expr::JuMP.AffExpr))
         DuckDB.end_row(appender)
     end
     return DuckDB.close(appender)
