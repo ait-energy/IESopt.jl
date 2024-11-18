e8e42f5aedba36c710109e2bb6932121ce748ceb
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-11-18 13:12:44 +0100

    fix: potential name leaking

--- src/precompile/precompile_tools.jl
+++ src/precompile/precompile_tools.jl
@@ -1,19 +1,19 @@
 @setup_workload begin
     # list = [...]
-    const dir = Assets.get_path("examples")
+    const __dir = Assets.get_path("examples")
 
     @compile_workload begin
-        model = generate!(normpath(dir, "01_basic_single_node.iesopt.yaml"))
+        model = generate!(normpath(__dir, "01_basic_single_node.iesopt.yaml"))
         optimize!(model)
 
-        generate!(normpath(dir, "08_basic_investment.iesopt.yaml"))
+        generate!(normpath(__dir, "08_basic_investment.iesopt.yaml"))
 
-        model = generate!(normpath(dir, "09_csv_only.iesopt.yaml"))
+        model = generate!(normpath(__dir, "09_csv_only.iesopt.yaml"))
         optimize!(model)
 
-        generate!(normpath(dir, "46_constants_in_objective.iesopt.yaml"))
+        generate!(normpath(__dir, "46_constants_in_objective.iesopt.yaml"))
 
-        fn = normpath(dir, "01_basic_single_node.iesopt.yaml")
+        fn = normpath(__dir, "01_basic_single_node.iesopt.yaml")
         generate!(fn; config=Dict("general.verbosity.core" => "debug"))
         generate!(fn; config=Dict("general.verbosity.core" => "info"))
         generate!(fn; config=Dict("general.verbosity.core" => "warn"))
