480eeb3309f0afa8991cef0b75ef2a80e93fa354
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-11-19 09:27:02 +0100

    fix: skip validation in parts of precompilation

--- src/precompile/precompile_tools.jl
+++ src/precompile/precompile_tools.jl
@@ -37,42 +37,43 @@
             ),
         )
         optimize!(model)
-        IESopt.run(fn; config)
-        generate!(normpath(__dir, "02_advanced_single_node.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "03_basic_two_nodes.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "04_soft_constraints.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "05_basic_two_nodes_1y.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "06_recursion_h2.iesopt.yaml"); config)
-        generate!(normpath(__dir, "07_csv_filestorage.iesopt.yaml"); config)
-        generate!(normpath(__dir, "08_basic_investment.iesopt.yaml"); config)
-        generate!(normpath(__dir, "09_csv_only.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "10_basic_load_shedding.iesopt.yaml"); config)
-        generate!(normpath(__dir, "11_basic_unit_commitment.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "12_incremental_efficiency.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "15_varying_efficiency.iesopt.yaml"); config)
-        generate!(normpath(__dir, "16_noncore_components.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "17_varying_connection_capacity.iesopt.yaml"); config)
+        IESopt.run(fn; config, skip_validation=true)
+        generate!(normpath(__dir, "02_advanced_single_node.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "03_basic_two_nodes.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "04_soft_constraints.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "05_basic_two_nodes_1y.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "06_recursion_h2.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "07_csv_filestorage.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "08_basic_investment.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "09_csv_only.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "10_basic_load_shedding.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "11_basic_unit_commitment.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "12_incremental_efficiency.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "15_varying_efficiency.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "16_noncore_components.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "17_varying_connection_capacity.iesopt.yaml"); config, skip_validation=true)
 
-        model = generate!(normpath(__dir, "18_addons.iesopt.yaml"); config)
+        model = generate!(normpath(__dir, "18_addons.iesopt.yaml"); config, skip_validation=true)
         get_components(model; tagged=["ModifyMe"])
 
-        model = generate!(normpath(__dir, "20_chp.iesopt.yaml"); config)
+        model = generate!(normpath(__dir, "20_chp.iesopt.yaml"); config, skip_validation=true)
         get_component(model, "chp")
 
-        # generate!(normpath(__dir, "22_snapshot_weights.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "23_snapshots_from_csv.iesopt.yaml"); config)
-        generate!(normpath(__dir, "25_global_parameters.iesopt.yaml"); config)
+        # generate!(normpath(__dir, "22_snapshot_weights.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "23_snapshots_from_csv.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "25_global_parameters.iesopt.yaml"); config, skip_validation=true)
         # generate!(
         #     normpath(__dir, "26_initial_states.iesopt.yaml");
         #     config,
         #     parameters=Dict("store_initial_state" => 15),
+        #     skip_validation=true,
         # )
-        # generate!(normpath(__dir, "27_piecewise_linear_costs.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "29_advanced_unit_commitment.iesopt.yaml"); config)
-        # generate!(normpath(__dir, "31_exclusive_operation.iesopt.yaml"); config)
+        # generate!(normpath(__dir, "27_piecewise_linear_costs.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "29_advanced_unit_commitment.iesopt.yaml"); config, skip_validation=true)
+        # generate!(normpath(__dir, "31_exclusive_operation.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "37_certificates.iesopt.yaml"); config=Dict("general.verbosity.core" => "error"))
-        generate!(normpath(__dir, "44_lossy_connections.iesopt.yaml"); config)
-        generate!(normpath(__dir, "47_disable_components.iesopt.yaml"); config)
+        generate!(normpath(__dir, "44_lossy_connections.iesopt.yaml"); config, skip_validation=true)
+        generate!(normpath(__dir, "47_disable_components.iesopt.yaml"); config, skip_validation=true)
     end
 
     # Clean up output files after testing is done.
