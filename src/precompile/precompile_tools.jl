3275025f8c21caa5fbdced31239fa85546575e46
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-11-20 12:33:31 +0100

    fix: properly close log files during precompile, which might fix issues under Windows

--- src/precompile/precompile_tools.jl
+++ src/precompile/precompile_tools.jl
@@ -7,9 +7,10 @@
         config = Dict("optimization.snapshots.count" => 3, "general.verbosity.core" => "error")
         fn = String(normpath(__dir, "01_basic_single_node.iesopt.yaml"))
 
-        generate!(fn; config=Dict("general.verbosity.core" => "debug"))
-        generate!(fn; config=Dict("general.verbosity.core" => "info"))
-        generate!(fn; config=Dict("general.verbosity.core" => "warn"))
+        safe_close_filelogger(generate!(fn; config=Dict("general.verbosity.core" => "debug")))
+        safe_close_filelogger(generate!(fn; config=Dict("general.verbosity.core" => "info")))
+        safe_close_filelogger(generate!(fn; config=Dict("general.verbosity.core" => "warn")))
+
         model = generate!(
             fn;
             config=Dict(
@@ -19,6 +20,8 @@
             ),
         )
         optimize!(model)
+        safe_close_filelogger(model)
+
         model = generate!(
             fn;
             config=Dict(
@@ -28,6 +31,8 @@
             ),
         )
         optimize!(model)
+        safe_close_filelogger(model)
+
         model = generate!(
             fn;
             config=Dict(
@@ -37,31 +42,48 @@
             ),
         )
         optimize!(model)
-        IESopt.run(fn; config, skip_validation=true)
-        generate!(normpath(__dir, "02_advanced_single_node.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(model)
+
+        safe_close_filelogger(IESopt.run(fn; config, skip_validation=true))
+
+        safe_close_filelogger(
+            generate!(normpath(__dir, "02_advanced_single_node.iesopt.yaml"); config, skip_validation=true),
+        )
         # generate!(normpath(__dir, "03_basic_two_nodes.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "04_soft_constraints.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "05_basic_two_nodes_1y.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "06_recursion_h2.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "07_csv_filestorage.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "08_basic_investment.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "09_csv_only.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(
+            generate!(normpath(__dir, "07_csv_filestorage.iesopt.yaml"); config, skip_validation=true),
+        )
+        safe_close_filelogger(
+            generate!(normpath(__dir, "08_basic_investment.iesopt.yaml"); config, skip_validation=true),
+        )
+        safe_close_filelogger(generate!(normpath(__dir, "09_csv_only.iesopt.yaml"); config, skip_validation=true))
         # generate!(normpath(__dir, "10_basic_load_shedding.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "11_basic_unit_commitment.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(
+            generate!(normpath(__dir, "11_basic_unit_commitment.iesopt.yaml"); config, skip_validation=true),
+        )
         # generate!(normpath(__dir, "12_incremental_efficiency.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "15_varying_efficiency.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "16_noncore_components.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(
+            generate!(normpath(__dir, "16_noncore_components.iesopt.yaml"); config, skip_validation=true),
+        )
         # generate!(normpath(__dir, "17_varying_connection_capacity.iesopt.yaml"); config, skip_validation=true)
 
         model = generate!(normpath(__dir, "18_addons.iesopt.yaml"); config, skip_validation=true)
         get_components(model; tagged=["ModifyMe"])
+        safe_close_filelogger(model)
 
         model = generate!(normpath(__dir, "20_chp.iesopt.yaml"); config, skip_validation=true)
         get_component(model, "chp")
+        safe_close_filelogger(model)
 
         # generate!(normpath(__dir, "22_snapshot_weights.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "23_snapshots_from_csv.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "25_global_parameters.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(
+            generate!(normpath(__dir, "25_global_parameters.iesopt.yaml"); config, skip_validation=true),
+        )
         # generate!(
         #     normpath(__dir, "26_initial_states.iesopt.yaml");
         #     config,
@@ -72,8 +94,12 @@
         # generate!(normpath(__dir, "29_advanced_unit_commitment.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "31_exclusive_operation.iesopt.yaml"); config, skip_validation=true)
         # generate!(normpath(__dir, "37_certificates.iesopt.yaml"); config=Dict("general.verbosity.core" => "error"))
-        generate!(normpath(__dir, "44_lossy_connections.iesopt.yaml"); config, skip_validation=true)
-        generate!(normpath(__dir, "47_disable_components.iesopt.yaml"); config, skip_validation=true)
+        safe_close_filelogger(
+            generate!(normpath(__dir, "44_lossy_connections.iesopt.yaml"); config, skip_validation=true),
+        )
+        safe_close_filelogger(
+            generate!(normpath(__dir, "47_disable_components.iesopt.yaml"); config, skip_validation=true),
+        )
     end
 
     # Clean up output files after testing is done.
