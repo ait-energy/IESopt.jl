147113b8c65677c456ac4fda2917d00cfff305d8
sstroemer <stefan.stroemer@gmail.com>
2024-12-11 01:10:41 +0100

    test: add tests for Connection delays

--- test/src/basic.jl
+++ test/src/basic.jl
@@ -16,7 +16,7 @@
     @test JuMP.termination_status(model) == JuMP.MOI.INFEASIBLE
 end
 
-@testset "Connection losses" begin
+@testset "Connection (loss)" begin
     model = generate!(joinpath(PATH_TESTFILES, "connection_loss.iesopt.yaml"))
     optimize!(model)
 
@@ -37,6 +37,26 @@ end
     @test JuMP.value(get_component(model, "conn3").var.flow[1]) ≈ 10.0 atol = 0.01
     @test JuMP.value(get_component(model, "conn4").var.flow[1]) ≈ 10.0 atol = 0.01
 end
+
+@testset "Connection (delay)" begin
+    model = generate!(joinpath(PATH_TESTFILES, "connection_delay.iesopt.yaml"))
+    IESopt.optimize!(model)
+
+    @test all(JuMP.value.(get_component(model, "conn1").exp.in) .≈ [1.0, 1.0, 1.0, 1.0])
+    @test all(JuMP.value.(get_component(model, "conn1").exp.out) .≈ [1.0, 1.0, 1.0, 1.0])
+
+    @test all(JuMP.value.(get_component(model, "conn2").exp.in) .≈ [1.0, 1.0, 0.0, 0.0])
+    @test all(JuMP.value.(get_component(model, "conn2").exp.out) .≈ [0.0, 0.0, 0.9, 0.9])
+
+    @test maximum(abs.(JuMP.value.(get_component(model, "conn3").exp.in) .- [1.111, 0.168, 1.111, 0.0])) <= 1e-3
+    @test maximum(abs.(JuMP.value.(get_component(model, "conn3").exp.out) .- [0.0, 1.0, 0.151, 0.999])) <= 1e-3
+
+    @test maximum(abs.(JuMP.value.(get_component(model, "conn4").exp.in) .- [0.0, 0.0, 1.054, 1.054])) <= 1e-3
+    @test maximum(abs.(JuMP.value.(get_component(model, "conn4").exp.out) .- [0.0, 0.0, 0.948, 0.948])) <= 1e-3
+
+    @test JuMP.objective_value(model) ≈ 10.4985 atol = 0.01
+end
+
 @testset "Versioning" begin
     v_curr = VersionNumber(string(pkgversion(IESopt))::String)
     v_good = [v_curr, VersionNumber(v_curr.major)]
