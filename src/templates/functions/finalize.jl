5eee767bc9d165d2f8a3e86ea0de3ba69dd0459f
Stefan Strömer <stefan.stroemer@ait.ac.at>
2025-06-23 18:50:51 +0200

    feat: manually create IDs for RGFs that may be cached, since that uses expression-to-string-to-sha1 internally which takes a long time

--- src/templates/functions/finalize.jl
+++ src/templates/functions/finalize.jl
@@ -7,11 +7,13 @@ function _build_template_function_finalize(template::CoreTemplate)
     # Get code from "finalize" and remove trailing newline.
     code = chomp(template.yaml["functions"]["finalize"])
 
+    rgf_id = Tuple(reinterpret(UInt32, SHA.sha1(chomp(code))))
+
     # Parse the code into an expression.
     code_ex = Meta.parse("""begin\n$(code)\nend"""; filename="$(template.name).iesopt.template.yaml")
 
     # Convert into a proper function.
-    template.functions[:finalize] = @RuntimeGeneratedFunction(
+    template.functions[:finalize] = _compile_rgf(
         :(function (__virtual__::Virtual)
             __template_name__ = $(template).name
             __parameters__ = __virtual__._parameters
@@ -27,7 +29,8 @@ function _build_template_function_finalize(template::CoreTemplate)
                 rethrow(e)
             end
             return nothing
-        end)
+        end);
+        id=rgf_id,
     )
 
     return nothing
