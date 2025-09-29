4e8f1360a4d52c0c21f6b4135d2d40d062cc5448
Daniel Schwabeneder <daniel.schwabeneder@ait.ac.at>
2025-09-29 18:12:51 +0200

    test: fix typo in example 1

--- test/src/examples.jl
+++ test/src/examples.jl
@@ -7,8 +7,8 @@
     TestExampleModule.check(; obj=675, components=Dict("co2_emissions" => Dict("cost" => 200)))
     # We can also disable components.
     # Zero demand results in zero cost.
-    TestExampleModule.check(; obj=0, componenents=Dict("demand.enabled" => false))
-    TestExampleModule.check(; obj=0, componenents=Dict("demand.disabled" => true))
+    TestExampleModule.check(; obj=0, components=Dict("demand.enabled" => false))
+    TestExampleModule.check(; obj=0, components=Dict("demand.disabled" => true))
 end
 
 @testitem "02_advanced_single_node" tags = [:examples] setup = [TestExampleModule] begin
